ddad2b01f1379d47e3853e3bc311105581dde123
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-11-28 22:34:40 +0300

    Update mysql.js

--- database/mysql.js
+++ database/mysql.js
@@ -1,7 +1,7 @@
 const sql = require("mysql")
 require("dotenv").config()
 
-const db = sql.createConnection({
+const db = sql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,

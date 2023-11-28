d28c24ed1537cadfd88e443e57a0ace5a8469d1c
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-11-28 17:39:54 +0300

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,1 +1,50 @@
-//soon
+const { addOrder, getOrders, getSingleOrder, updateStatus, deleteOrder } = require("../controller/orders");
+
+const router = require("express").Router();
+
+const path = require("path");
+
+const checkFileType = function (file, cb) {
+
+  const fileTypes = /pdf/;
+
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error: You can Only Upload files!!");
+  }
+};
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../client/src/assets/orders/bills");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+    checkFileType
+  },
+});
+
+const upload = multer({ 
+  storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+}).single("bill");
+
+
+
+
+router.post("/order",upload, addOrder)
+router.get("/get-orders", getOrders)
+router.get("/get-single-order/:id", getSingleOrder)
+router.put("/change-status/:id", updateStatus)
+router.post("/delete-order/:id", deleteOrder)
+
+module.exports = {router}

69922f9e924edd3420e591c48394fb53d868ebdb
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-07 04:06:14 +0300

    Add files via upload

---
+++ models/orders.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const uploadBill = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    bill:{
+        type:[String],
+        required:true
+    }
+})
+
+const ordersModel = mongoose.model("uploadedbills", uploadBill)
+module.exports = ordersModel
---
+++ models/products.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+
+const uploadImage = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    imageone:{
+        type:[String],
+        required:true,
+    },
+    imagetwo:{
+        type:[String],
+        required:true,
+    }
+})
+
+const productsModel = mongoose.model("uploadedImages", uploadImage)
+module.exports = productsModel

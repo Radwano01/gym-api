61d07b8863bb00acc55e262de726dad9aa78b1ca
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-07 04:03:59 +0300

    Add files via upload

---
+++ database/mysql.js
@@ -0,0 +1,17 @@
+const sql = require("mysql")
+require("dotenv").config()
+
+const db = sql.createConnection(process.env.DATABASE_URL)
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      return;
+    }
+    console.log('Connected to mysql');
+});
+
+module.exports = {
+    db
+}
+

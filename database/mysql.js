e7382d2a8a9bf8e34cbbd9b1b9cc9b2be4ba0625
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-08 15:56:21 +0300

    Update mysql.js

--- database/mysql.js
+++ database/mysql.js
@@ -1,10 +1,7 @@
 const sql = require("mysql")
 require("dotenv").config()
 
-const db = sql.createPool({
-    process.env.DATABASE_URL,
-    connectionLimit: 10,
-})
+const db = sql.createConnection(process.env.DATABASE_URL)
 
 module.exports = {
     db

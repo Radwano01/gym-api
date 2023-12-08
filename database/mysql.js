d53aba522a0dde0eae61b2e5399d8bf1d95f50fc
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-08 15:39:54 +0300

    Update mysql.js

--- database/mysql.js
+++ database/mysql.js
@@ -3,14 +3,6 @@ require("dotenv").config()
 
 const db = sql.createPool(process.env.DATABASE_URL)
 
-db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to database:', err);
-      return;
-    }
-    console.log('Connected to mysql');
-});
-
 module.exports = {
     db
 }

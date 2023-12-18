1c41e612fb221c1343f74c8f9fd9788d04245549
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-12-19 01:20:39 +0300

    Update mysql.js

--- database/mysql.js
+++ database/mysql.js
@@ -1,7 +1,17 @@
 const sql = require("mysql")
 require("dotenv").config()
 
-const db = sql.createConnection(process.env.DATABASE_URL)
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  connectTimeout: parseInt(process.env.CONNECT_TIMEOUT || "10000", 10),
+  port: parseInt(process.env.PORT, 10),
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
 
 module.exports = {
     db
